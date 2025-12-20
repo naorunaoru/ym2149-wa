9f3355777b5af339af8af55a785d5cea6ab0f39c
Roman Kuraev <naoru.desu@gmail.com>
2025-12-20 03:12:48 +0000

    fix: stupid link

--- packages/demo/src/components/Footer/Footer.tsx
+++ packages/demo/src/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
       YM2149 PSG emulation using Web Audio API
       <br />
-      <a href="https://github.com/user/ym2149-wa" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/naorunaoru/ym2149-wa"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         View on GitHub
       </a>
     </footer>
